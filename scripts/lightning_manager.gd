d8563c3ef40c154bba2671337e34c6470c5a9a37
ChapoTheGoat <gama_giovanni@yahoo.com>
2025-05-13 21:14:48 -0500

    Thunder Tweaks Adding Lightning Pattern Functions

--- scripts/lightning_manager.gd
+++ scripts/lightning_manager.gd
@@ -13,18 +13,18 @@ class_name Lightning_Manager extends Node3D
 	preload('res://assets/sfx/Thunder_Sounds/Distant/mixkit-thunderstorm-background-sound-2398.wav'),]
 @onready var close_thunder_audio_pool := [preload("res://assets/sfx/Thunder_Sounds/Close/mixkit-nature-ambience-with-lightning-strike-and-thunder-3093.wav"),
 preload("res://assets/sfx/Thunder_Sounds/Close/mixkit-thunder-with-rain-in-the-storm-1294.wav")]
-
 var tween : Tween
 
 func _ready():
 	change_to_env(dark_env,0.0,0.0)
-	start_sequence_lightning()
+	lightning_pattern_two()
 	
 func start_sequence_lightning():
+	print("Pattern 1")
 	directional_light.rotation = Vector3(250,randf_range(-360, 360),0)
-	var subtle_flash_timing = randf_range(0.01,0.15)
+	var subtle_flash_timing = randf_range(0.01,0.2)
 	
-	for i in randi_range(0,2):
+	for i in randi_range(0,1.5):
 		await change_to_env(subtle_flash_env,subtle_flash_timing,0.05)
 		await change_to_env(dark_env,0.2,0.01)
 		
@@ -42,20 +42,41 @@ func start_sequence_lightning():
 		
 	await change_to_env(subtle_flash_env,randf_range(0.7,1.0),0.05)
 	await change_to_env(dark_env,randf_range(0.05,0.2),0.0)
-	play_thunder_audio()
-	await get_tree().create_timer(randf_range(3.0,6.0)).timeout
+	play_thunder_audio(false)
+	await get_tree().create_timer(randf_range(2.5,5.0)).timeout
+	lightning_pattern_two()
+	
+func lightning_pattern_two():
+	print("Pattern 2")
+	directional_light.rotation = Vector3(250,randf_range(-360, 360),0)
+	var subtle_flash_timing = randf_range(0.01,0.1)
+	
+	for i in randi_range(0,3):
+		await change_to_env(subtle_flash_env,subtle_flash_timing,0.02)
+		await change_to_env(dark_env,0.2,0.01)
+		
+	await change_to_env(big_flash_env,subtle_flash_timing,0.05)
+	for i in randi_range(0,1):
+		await change_to_env(big_flash_env,subtle_flash_timing,0.9)
+	play_thunder_audio(true)
+	await get_tree().create_timer(randf_range(0.5,2.0)).timeout
 	start_sequence_lightning()
 	
-func play_thunder_audio():
-	var sound_lag = randf_range(0.5,1.5)
+func play_thunder_audio(force_play_close_audio : bool):
+	var sound_lag = randf_range(0.2,1.5)
 	var sound : AudioStream
 	var volume : float = -sound_lag * 3.0
 	
-	if sound_lag >= 1.0:
+	if force_play_close_audio:
+		sound = close_thunder_audio_pool.pick_random()
+		print("Close")
+	elif sound_lag >= .5:
 		sound = distant_thunder_audio_pool.pick_random()
+		print("Distant")
 	else:
 		sound = close_thunder_audio_pool.pick_random()
-		
+		print("Close")
+	
 	var thunder_audio_player = AudioStreamPlayer.new()
 	get_parent().add_child(thunder_audio_player)
 	
