ccd84932e296f9a59a2b7925130589398f76f92f
lethalgem <iwc123@gmail.com>
2025-05-17 00:15:10 -0400

    Update scripts/audio_slider.gd

--- scripts/audio_slider.gd
+++ scripts/audio_slider.gd
@@ -2,7 +2,7 @@ class_name AudioSlider extends HSlider
 
 @export var bus_name: String
 var bus_index: int
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	bus_index = AudioServer.get_bus_index(bus_name)
 	
