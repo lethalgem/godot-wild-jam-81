066264b4fe558bedcf73177738897df0a0c2772a
lethalgem <iwc123@gmail.com>
2025-05-18 13:50:17 -0400

    More tweaking

--- scripts/lightning_manager.gd
+++ scripts/lightning_manager.gd
@@ -13,14 +13,14 @@ class_name Lightning_Manager extends Node3D
 
 var tween : Tween
 var max_directional_light_energy := 6.0
-var max_sky_light_energy := 0.5
+var max_sky_light_energy := 1.0
 
 func _ready():
 	change_to_env(0.0,0.0,0.0)
 	
 	while true:
 		await start_sequence_lightning()
-		await get_tree().create_timer(randf_range(2.5,5.0)).timeout
+		await get_tree().create_timer(randf_range(6.0, 14.0)).timeout
 
 func start_sequence_lightning():
 	directional_light.rotation = Vector3(directional_light.rotation.x,deg_to_rad(randf_range(-360, 360)),directional_light.rotation.z)
@@ -30,7 +30,7 @@ func start_sequence_lightning():
 	var subtle_light_max_factor := 0.05
 	var buildup_sky_light_energy_inc := max_sky_light_energy * randf_range(subtle_light_min_factor,subtle_light_max_factor)
 	var buildup_dir_light_energy_inc := max_directional_light_energy * randf_range(subtle_light_min_factor,subtle_light_max_factor)
-	var sky_light_energy := 0.025
+	var sky_light_energy := 0.1
 	var dir_light_energy := 0.025
 	for i in randi_range(1, 4):
 		await change_to_env(sky_light_energy, 0.05, dir_light_energy)
@@ -43,16 +43,21 @@ func start_sequence_lightning():
 	await change_to_env(0.0,randf_range(0.8, 1.3),0.0)
 	
 	# big bang
-	await change_to_env(max_sky_light_energy, 0.05, max_directional_light_energy)
-	await get_tree().create_timer(randf_range(0.1, 0.3)).timeout
-	await change_to_env(0.0,randf_range(0.8, 1.3),0.0)
+	var is_big_bang = randi_range(0, 1)
+	for i in is_big_bang:
+		await change_to_env(max_sky_light_energy, 0.05, max_directional_light_energy)
+		await get_tree().create_timer(randf_range(0.1, 0.3)).timeout
+		await change_to_env(0.0,randf_range(0.8, 1.3),0.0)
 	
-	play_thunder_audio(false)
+	if is_big_bang == 1:
+		play_thunder_audio(true)
+	else:
+		play_thunder_audio(false)
 	
 func play_thunder_audio(force_play_close_audio : bool):
 	var sound_lag = randf_range(0.2,1.5)
 	var sound : AudioStream
-	var volume : float = -sound_lag * 3.0 - 15.0
+	var volume : float = -sound_lag * 3.0 - 7.0
 	
 	if force_play_close_audio:
 		sound = close_thunder_audio_pool.pick_random()
